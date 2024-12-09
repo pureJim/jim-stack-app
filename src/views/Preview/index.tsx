723c3b3484ac5b3f46b5295941caeb8a438aa1de
Jim <jim@cn.purefda.com>
2024-12-10 00:44:52 +0800

    chore: font

--- src/views/Preview/index.tsx
+++ src/views/Preview/index.tsx
@@ -1,5 +1,13 @@
 const PreviewPage = () => {
-  return <div>preview</div>;
+  return (
+    <div>
+      previewadsdadsa
+      <p>2132132132</p>
+      <p style={{ fontFamily: 'var(--font-bold)' }}>今天天气怎么样 - Ce是  测试</p>
+      <h3>123123</h3>
+      <h2>测试一下</h2>
+    </div>
+  );
 };
 
 export default PreviewPage;
